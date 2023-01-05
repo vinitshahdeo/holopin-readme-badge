4d8b7cdf5087290cb2f8c8612516da603e90a65e
Vinit Shahdeo <vinitshahdeo@gmail.com>
2023-01-05 18:51:20 +0530

    Added a boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work in Progress | Holopin README Badges Generator
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/vinitshahdeo/holopin-readme-badge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about Holopin Badges
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
